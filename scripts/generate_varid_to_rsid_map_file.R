890064fababa362ecbe28a1dcb590e0ed9305ecc
Claire Kim <35745275+hyunkyungclairekim@users.noreply.github.com>
2021-06-16 16:07:38 -0400

    Update generate_varid_to_rsid_map_file.R

--- scripts/generate_varid_to_rsid_map_file.R
+++ scripts/generate_varid_to_rsid_map_file.R
@@ -1,23 +1,20 @@
 
 CURRENT <- paste(getwd(),"/",sep="")
 
-
+# download all variant information for homo sapiens GRCh37 from Ensembl
 url <- "http://ftp.ensembl.org/pub/grch37/current/variation/vcf/homo_sapiens/1000GENOMES-phase_3.vcf.gz"
 destfile <- paste(CURRENT,"1000GENOMES-phase_3.vcf.gz",sep="")
- 
 download.file(url, destfile)
-
 
 library(vcfR)
 vcf <- read.vcfR(destfile)
-
 vars = vcf@fix
 
+# format output file
 vars = as.data.frame(vars[,c("CHROM","POS","ID","REF","ALT")])
 vars$VAR_ID = paste(vars$CHROM,vars$POS,vars$REF,vars$ALT,sep="_")
 vars = vars[,c("VAR_ID","ID")]
 names(vars) = c("VAR_ID","rsID")
-
 
 write.table(vars,file=paste(CURRENT,"VARID_rsID_map_file.txt",sep=""), append = F, quote = F, sep = "\t",
             eol = "\n", na = "NA", dec = ".", row.names = F,
